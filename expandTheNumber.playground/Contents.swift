072149837fe9b561fc4d050831e0363e711f4c1f
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-12 16:49:28 +0000

    Challenge solved

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,29 @@
 import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
+    let numberString = String(number)
+    var digitArray = numberString.compactMap({ $0.wholeNumberValue })
     
+    switch digitArray.count {
+    case 3:
+        let first = digitArray[0] * 100
+        let second = digitArray[1] * 10
+        
+        digitArray[0] = first
+        digitArray[1] = second
+    case 4:
+        let first = digitArray[0] * 1000
+        let second = digitArray[1] * 100
+        let third = digitArray[2] * 10
+        
+        digitArray[0] = first
+        digitArray[1] = second
+        digitArray[2] = third
+    default:
+        digitArray
+    }
+    
+    return digitArray
 }
 
 expandTheNumber(199)  // [100, 90, 9]
